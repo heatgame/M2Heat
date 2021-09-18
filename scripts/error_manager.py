b476a6f06604c3d4d5f8ab359b8facddc3b579bc
corrupti-on <corruption@heatgame.dev>
2021-09-18 14:29:49 +0300

    Update error_manager.py

--- scripts/error_manager.py
+++ scripts/error_manager.py
@@ -4,7 +4,7 @@ import os, sys
 def push(error, delay = 15.0):
 	exc_type, exc_obj, exc_tb = sys.exc_info()
 	fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-	logger.trace(str(exc_type) + ", " + str(error) + ", " + str(fname) + ", " + str(exc_tb.tb_lineno))
+	logger.error(str(exc_type) + ", " + str(error) + ", " + str(fname) + ", " + str(exc_tb.tb_lineno))
 	
 	# try:
 	# 	import time
